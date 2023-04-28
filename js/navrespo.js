6c2d54e1d85a362ecf50c7df5e02dd232581f814
Jeniffer <78547309+Genunis@users.noreply.github.com>
2023-04-28 17:00:25 -0300

    responsivo

---
+++ js/navrespo.js
@@ -0,0 +1,23 @@
+// Menu Toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.links-container');
+const navbar = document.querySelector('.navbar');
+const logo = document.querySelector('.logo');
+const nav_container = document.querySelector('.nav-container');
+
+menuToggle.addEventListener('click', function() {
+  menuToggle.classList.toggle('active');
+  menu.classList.toggle('active');
+  navbar.classList.toggle('active');
+  logo.classList.toggle('active');
+  nav_container.classList.toggle('active');
+  if(document.body.style.overflow == 'hidden'){
+        document.body.style.overflow = 'auto';
+  }else{
+    document.body.style.overflow = 'hidden';
+  }
+});
+
+
+
+
